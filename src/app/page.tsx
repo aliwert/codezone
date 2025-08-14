623bdfc56e9ce994d7b04fd086b86fe0d245a5d2
aliwert <154356044+aliwert@users.noreply.github.com>
2025-08-14 15:17:21 +0300

    feat: change folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import TrendsSection from "@/components/TrendsSection";
-import HeroSection from "../components/HeroSection";
-import TwitchHeroSection from "../components/TwitchHeroSection";
-import FavoritesOfMonths from "@/components/FavoritesOfMonths";
-import DiscoverySection from "@/components/DiscoverySection";
+import TrendsSection from "@/components/desktop/TrendsSection";
+import HeroSection from "../components/desktop/HeroSection";
+import TwitchHeroSection from "../components/desktop/TwitchHeroSection";
+import FavoritesOfMonths from "@/components/desktop/FavoritesOfMonths";
+import DiscoverySection from "@/components/desktop/DiscoverySection";
 
 export default function Home() {
   return (
