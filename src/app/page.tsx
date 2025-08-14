b4836119717770887c49734ad7dd731d45dfe438
aliwert <154356044+aliwert@users.noreply.github.com>
2025-08-14 15:23:02 +0300

    fix vercel deploy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import TrendsSection from "@/components/desktop/TrendsSection";
+import TrendsSection from "../components/desktop/TrendsSection";
 import HeroSection from "../components/desktop/HeroSection";
 import TwitchHeroSection from "../components/desktop/TwitchHeroSection";
-import FavoritesOfMonths from "@/components/desktop/FavoritesOfMonths";
-import DiscoverySection from "@/components/desktop/DiscoverySection";
+import FavoritesOfMonths from "../components/desktop/FavoritesOfMonths";
+import DiscoverySection from "../components/desktop/DiscoverySection";
 
 export default function Home() {
   return (
